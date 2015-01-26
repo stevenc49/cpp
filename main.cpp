99d7ee9f11d7ad5de853e8feb45ae7cc7a3f467a
Steven Cheong <stevenc49@gmail.com>
2015-01-26 15:12:38 -0800

    struct assignment

--- main.cpp
+++ main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
 
+struct btreenode {
+
+    int data;
+    struct btreenode* left;
+    struct btreenode* right;
+
+};
+
 int main() {
 
-    cout << "test" << endl;
+    struct btreenode root;
+    root.data = 2;
+    root.left = 0;
+    root.right = 0;
+
+    struct btreenode left;
+    left.data = 1;
+
+    //root.left = (struct btreenode) malloc(sizeof(left));
+    root.left = &left;
+
+    cout << root.left << endl;
+    cout << root.right << endl;
 
     cin.get();
 
