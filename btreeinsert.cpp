41a53298d02a7342fac3089dc2e9cccd45958437
Steven Cheong <stevenc49@gmail.com>
2015-01-26 16:13:43 -0800

    better file names

---
+++ btreeinsert.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+typedef struct btreenode {
+
+    int data;
+    struct btreenode* left;
+    struct btreenode* right;
+
+} node;
+
+int main() {
+
+    node root;
+    root.data = 2;
+    root.left = 0;
+    root.right = 0;
+
+    node left;          //on the stack
+    left.data = 1;
+    root.left = &left;
+
+    node* right = (node*) malloc(sizeof(node));     //on the heap
+    right->data = 3;
+    root.right = (node*) right;
+
+    cout << root.left->data << endl;
+    cout << root.right->data << endl;
+
+    //cout << sizeof(struct btreenode) << endl;
+
+    cin.get();
+
+}
