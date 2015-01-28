b986abf560b2760b4ea3366c581be6abff3b2e9b
Steven Cheong <stevenc49@gmail.com>
2015-01-27 23:18:54 -0800

    atoi

--- atoi.cpp
+++ atoi.cpp
@@ -23,8 +23,28 @@ int atoi(const char* str) {
     int sum = 0;
     int str_size = size(str);
 
+    //set ptr to last char
+    //char ptr = str[str_size-1];
+    //sum += (int) ptr-'0';
 
-    cout << str[str_size-1] << endl;
+    int multiplier = 1;
+
+    for(int i=str_size-1; i>=0; i--) {
+
+
+
+        char ptr = str[i];
+        //cout << ptr << endl;
+
+        int toAdd = (int) (ptr-'0' * multiplier);
+        cout << "toAdd" << toAdd << endl;
+        sum += toAdd;
+
+        multiplier*=10;
+        cout << "mult" << multiplier<<endl;
+    }
+
+    //cout << sum << endl;
 
     //char* end = str[size(new_str)];
 
