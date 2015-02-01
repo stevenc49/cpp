fa572df481c08b2c346569aad5630df0b2353c92
Steven Cheong <stevenc49@gmail.com>
2015-02-01 15:58:51 -0800

    tree built

--- btreeSumLeftNodes.cpp
+++ btreeSumLeftNodes.cpp
@@ -24,7 +24,17 @@ int main() {
 
     //node* root = (node*) malloc(sizeof(node));
 
-    Node* root = newNode(2);
+    Node* root = newNode(20);
+
+    root->left = newNode(9);
+    root->left->left= newNode(5);
+    root->left->right = newNode(12);
+    root->left->right->right = newNode(15);
+
+    root->right = newNode(49);
+    root->right->left = newNode(23);
+    root->right->right = newNode(52);
+    root->right->right->left = newNode(50);
 
     return 0;
 
